9e45b8fd0069b537bdcd8d736861ec5141d441f9
Mohammed Zeeshan <93642091+alizeeshan1234@users.noreply.github.com>
2025-04-02 03:22:33 +0530

    Update spl_transfer.ts

--- cluster1/spl_transfer.ts
+++ cluster1/spl_transfer.ts
@@ -1,28 +1,50 @@
-import { Commitment, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js"
+import { Commitment, Connection, Keypair, PublicKey } from "@solana/web3.js";
 import { getOrCreateAssociatedTokenAccount, transfer } from "@solana/spl-token";
+import { BN } from "@coral-xyz/anchor";
 const wallet = require("./wallet.json");
 
 // We're going to import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
 
-//Create a Solana devnet connection
+// Create a Solana devnet connection
 const commitment: Commitment = "confirmed";
 const connection = new Connection("https://api.devnet.solana.com", commitment);
 
 // Mint address
-const mint = new PublicKey("<mint address>");
+const mint = new PublicKey("AddvSPsJSLN1wrXFT71TAEvnoV2SHzPVFZPwrev2kgu7");
 
-// Recipient address
-const to = new PublicKey("<receiver address>");
+const toWalletAddress = new PublicKey("FEZhZZCPu7xTBwupZhCFHzjmhaJ8mVVtdrrLc7n1DquJ");
 
 (async () => {
     try {
         // Get the token account of the fromWallet address, and if it does not exist, create it
+        let fromWallet = await getOrCreateAssociatedTokenAccount(
+            connection,
+            keypair,
+            mint,
+            keypair.publicKey,
+            true,
+            commitment
+        );
 
         // Get the token account of the toWallet address, and if it does not exist, create it
+        let toWallet = await getOrCreateAssociatedTokenAccount(
+            connection,
+            keypair,
+            mint,
+            toWalletAddress,
+            true,
+            commitment
+        );
 
-        // Transfer the new token to the "toTokenAccount" we just created
-    } catch(e) {
-        console.error(`Oops, something went wrong: ${e}`)
+        const amount = new BN(10); // amount to transfer
+
+        // Transfer the token to the recipient's token account
+        let transferTx = await transfer(connection, keypair, fromWallet.address, toWallet.address, keypair, amount);
+        console.log(transferTx);
+
+        console.log("Transfer successful!");
+    } catch (e) {
+        console.error(`Oops, something went wrong: ${e}`);
     }
 })();
